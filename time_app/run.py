931e6a42927cbbef41b922ac91cc96c45d81694c
ThisZiyu <146238439+ThisZiyu@users.noreply.github.com>
2023-10-05 14:43:42 -0400

    Update run.py

--- time_app/run.py
+++ time_app/run.py
@@ -10,8 +10,8 @@ def hello():
 @app.route('/time')
 def return_time():
     current_time = datetime.now()
-    time_str = current_time.strftime('%Y-%m-%d %H:%M:%S')  # 格式化时间为字符串
-    return jsonify({'The current time is': time_str})  # 将时间字符串转换为 JSON 格式并返回
+    time_str = current_time.strftime('%Y-%m-%d %H:%M:%S') 
+    return jsonify({'The current time is': time_str})  
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',
